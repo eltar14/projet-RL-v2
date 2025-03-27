3edb12fb286d2480f8a34cff0244a863b45f30ca
eltar14 <lebouant2@gmail.com>
2025-03-27 16:58:36 -0400

    cleaned main

--- main.py
+++ main.py
@@ -1,21 +1,12 @@
 import SnakeEnv
 from utils import load_model_state, run_simple_visual_test
-#snake = SnakeEnv.SnakeEnv()
-
-MOV_DICT = {
-    "UP":0,
-    "RIGHT":1,
-    "DOWN":2,
-    "LEFT":3,
-}
+from pygame_display import run_pygame_visual
 def demo_console():
-    model_path = "models/checkpoint - Copie (5).pth"
+    model_path = "models/demo_model_1.pth"
     run_simple_visual_test(model_path)
 
 def demo_pygame():
-    from pygame_display import run_pygame_visual
-
-    model_path = "models/checkpoint - Copie (5).pth"  # update this path if needed
+    model_path = "models/demo_model_1.pth"
     run_pygame_visual(model_path, fps=5, width=10, height=10, record_gif=True)
 
 
