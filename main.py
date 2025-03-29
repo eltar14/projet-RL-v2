96c12ee9410e218b4ca3f8179fd2eec0517e8526
eltar14 <lebouant2@gmail.com>
2025-03-28 21:10:50 -0400

    demo model (weights only)

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ import SnakeEnv
 from utils import load_model_state, run_simple_visual_test
 from pygame_display import run_pygame_visual
 def demo_console():
-    model_path = "models/demo_model_1.pth"
+    model_path = "models/demo_model.pth"
     run_simple_visual_test(model_path)
 
 def demo_pygame():
-    model_path = "models/demo_model_1.pth"
+    model_path = "models/demo_model.pth"
     run_pygame_visual(model_path, fps=5, width=10, height=10, record_gif=True)
 
 
